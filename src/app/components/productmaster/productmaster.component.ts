3ae42b97e8abc569ec16a902542b1a6ba5674b31
saumyasingh20 <saumyaaaa98@gmail.com>
2022-07-27 14:28:58 +0530

    update in functionality

--- src/app/components/productmaster/productmaster.component.ts
+++ src/app/components/productmaster/productmaster.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductmasterService } from 'src/app/services/productmaster.service';
 import { Product } from 'src/app/model/Product';
+import { resetFakeAsyncZone } from '@angular/core/testing';
 @Component({
   selector: 'app-productmaster',
   templateUrl: './productmaster.component.html',
   styleUrls: ['./productmaster.component.css']
 })
 export class ProductmasterComponent implements OnInit {
+  isAdded:boolean=false;
   isTableVisible:boolean=false;
+  isResetVisible:boolean=false;
   prod:Product={
     id:0,
     name:'',
@@ -15,30 +18,44 @@ export class ProductmasterComponent implements OnInit {
     manu:'',
     price:0
   }
-  products:Product[]=[];
+   products:Product[]=[];
 
   constructor(private productmasterservice:ProductmasterService) { }
 
   ngOnInit(): void {}
+  
   displayProducts():void{
+   
     this.isTableVisible=true;
-    this.productmasterservice.getProducts().subscribe((data:any[])=>{
+    this.productmasterservice.getProducts().subscribe(
+     (data:any[])=>{
       console.log(data)
       this.products = data
-    })
+     })
+    
   }
   addProduct(): void{
     this.productmasterservice.addProduct(this.prod).subscribe(
       prod =>this.products.unshift(prod)
    )
-    this.displayProducts();
-   this.prod={
-    id:0,
-    name:'',
-    desc:'',
-    manu:'',
-    price:0
+   this.isAdded=true;
+   this.displayProducts();
+   this.isResetVisible=true;
+   
   }
+  resetData():void{
+    this.prod={
+      id:0,
+      name:'',
+      desc:'',
+      manu:'',
+      price:0
+    }
+    this.isAdded=false;
+    this.isResetVisible=false;
+    this.isTableVisible=false;
+
   }
+  
 
 }
